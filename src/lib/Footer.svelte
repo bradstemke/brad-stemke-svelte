6734b58d3d00081a3bc12ce0c497a90eeb5ee99e
Brad Stemke <bradstemke@bradsmbpm3pro.lan>
2024-01-21 22:26:10 -0800

    Added some global css vars and some typography styles, created the Footer component after converting it from a React component into Svelte

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+  const socialLinks: { title: string, url: string}[] = [
+    {
+      title: "Github",
+      url: "https://github.com/bradstemke",
+    },
+    {
+      title: "Instagram",
+      url: "http://instagram.com/bradstemke",
+    },
+    {
+      title: "Twitter",
+      url: "https://twitter.com/bradstemke",
+    }
+  ];
+</script>
+
+<footer>
+  <div class="inner">
+    <div class="linklist-wrapper">
+      <ul>
+        {#each socialLinks as link}
+          <li>
+            <a href={link.url} target="_blank">{link.title}</a>
+          </li>
+        {/each }
+      </ul>  
+    </div>
+
+    <div class="copyright">
+      <p>©{new Date().getFullYear()}</p>
+    </div>
+  </div>
+</footer>
+
+<style lang="css">
+
+footer {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-primary-tan);
+}
+
+@media only screen and (min-width: 900px) {
+  footer {
+    flex-direction: row;
+    padding: var(--spacing-3);
+  }
+}
+
+.inner {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: var(--spacing-2) var(--spacing-2) var(--spacing-4);
+}
+
+@media only screen and (min-width: 900px) {
+  .inner {
+    flex-direction: row;
+    width: 100%;
+    padding: var(--spacing-2) var(--spacing-3);
+  }
+}
+
+.copyright {
+  text-align: center;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+}
+
+@media only screen and (min-width: 900px) {
+  .copyright {
+    justify-content: flex-end;
+  }
+}
+
+.linklist-wrapper {
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-content: center;
+  margin: 0 0 var(--spacing-4) 0;
+}
+
+@media only screen and (min-width: 900px) {
+  .linklist-wrapper {
+    justify-content: flex-start;
+    margin: 0;
+  }
+}
+
+ul li {
+  display: inline-block;    
+  background-color: var(--color-primary-green);
+  margin-right: var(--spacing-2);
+}
+
+@media only screen and (min-width: 900px) {
+  ul li {
+    margin-right: var(--spacing-1);
+  }
+}
+
+ul li:last-of-type {
+  margin-right: 0;
+}
+
+ul li:focus, ul li:hover, ul li:focus-visible {
+  transform: rotateZ(6deg);
+  outline-color: var(--color-primary-yellow);
+}
+
+ul li:nth-of-type(even):focus, ul li:nth-of-type(even):hover, ul li:nth-of-type(even):focus-visible {
+  transform: rotateZ(-6deg);
+}
+
+ul li a {
+  display: block;
+  color: var(--color-primary-white);
+  text-decoration: none;
+  text-transform: uppercase;
+  padding: var(--spacing-1);
+}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Footer from "$lib/Footer.svelte";
+</script>
+
+<h1>Brad Stemke</h1>
+
+<Footer />
+
+<style>
+  :root {
+    /* Colors */
+    --color-primary-yellow: rgb(243, 175, 89);
+    --color-primary-tan: rgb(248, 247, 243);
+    --color-primary-red: rgb(189, 75, 47);
+    --color-primary-blue: rgb(30, 74, 159);
+    --color-primary-green: rgb(38, 77, 68);
+    --color-primary-white: rgb(248, 245, 241);
+
+    /* Spacing  */
+    --spacing-1: 0.5rem;
+    --spacing-2: 1rem;
+    --spacing-3: 1.5rem;
+    --spacing-4: 2rem;
+    --spacing-5: 2.5rem;
+    --spacing-6: 3rem;
+    --spacing-7: 3.5rem;
+    --spacing-8: 4rem;
+
+    /* Typography */
+    font-family: 'Space Mono', monospace;
+    font-size: 16px;
+    font-variant-ligatures: none;
+    line-height: 1.4;  
+  }
+  
+  /* List Resets  */
+  :global(ul, ol) {
+    margin: 0;
+    padding: 0;
+  } 
+</style>
